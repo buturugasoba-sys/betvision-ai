c99e13b4c922d71b99ffb46db1df25b1c8a33ce4
buturugasoba-sys <buturugasoba@gmail.com>
2026-07-10 15:49:40 +0300

    Connect frontend with API

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+async function analyzeMatch() {
+    const response = await fetch("http://localhost:8000/analyze", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            home_form: 80,
+            goals_average: 3,
+            defense_strength: 75
+        })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+}
